426c6b6e12fccc3f7a9f03dd892e57ee34277212
Stephen Early <steve@assorted.org.uk>
2020-12-13 11:51:23 +0000

    Day 13: use python 3.8 built-in modular inverse

--- day13.py
+++ day13.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# NB requires python 3.8 for modular inverse
+
 with open("day13-input.txt") as f:
     earliest = int(f.readline())
     schedule = [ None if x == 'x' else int(x) for x in f.readline().split(',') ]
@@ -27,25 +29,12 @@ c = [ (bus, bus - offset)
 
 from functools import reduce
 
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-    if b == 1:
-        return 1
-    while a > 1:
-        q = a // b
-        a, b = b, a % b
-        x0, x1 = x1 - q * x0, x0
-    if x1 < 0:
-        x1 += b0
-    return x1
- 
 def chinese_remainder(c):
     s = 0
     prod = reduce(lambda a, b: a * b, (x[0] for x in c))
     for n, a in c:
         p = prod // n
-        s += a * mul_inv(p, n) * p
+        s += a * pow(p, -1, n) * p
     return s % prod
 
 print(f"Part two: {chinese_remainder(c)}")
