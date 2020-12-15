f1338ae15edafc9c8ffbc6f6e131daee41daa1f0
Stephen Early <steve@assorted.org.uk>
2020-12-15 10:14:50 +0000

    Day 15

---
+++ day15.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+i = [5, 2, 8, 16, 18, 0, 1]
+
+import itertools
+
+def mem(start):
+    start = list(start)
+    turn = 0
+    h = {}
+    while True:
+        if start:
+            num = start.pop(0)
+        else:
+            last_a, last_b = h.get(num, (0, 0))
+            num = last_a - last_b
+        yield num
+        h[num] = (turn, h.get(num, (turn, turn))[0])
+        turn += 1
+
+def th(n, s):
+    for i in range(n):
+        r = next(s)
+    return r
+
+print(f"Part one: {th(2020, mem(i))}")
+print(f"Part two: {th(30000000, mem(i))}")
